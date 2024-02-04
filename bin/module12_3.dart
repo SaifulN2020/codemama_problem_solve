87567d7e0e954b534937295f942922015a3cdfe1
saiful <saiful0687@gmail.com>
2024-02-04 13:12:07 -0500

    module 13 complete

--- bin/module12_3.dart
+++ bin/module12_3.dart
@@ -1,14 +1,31 @@
 import 'dart:io';
-void main() {
-  List<int> input = stdin.readLineSync()!.split(' ').map((e) => int.parse(e)).toList();
-  int start = input[0];
-  int end = input[1];
 
-  int duration;
-  if (start <= end) {
-    duration = end - start;
-  } else {
-    duration = 24 - start + end;
-  }
+void main() {
+
+
+  List<String> startTime = stdin.readLineSync()!.split(' ');
+  int startTimeTime =int.parse(startTime[0]);
+  int endTime=int.parse(startTime[1]);
+
+
+
+  // Calculate duration
+  int duration = calculateDuration(startTimeTime, endTime);
+
+  // Output the result
   print(duration);
+}
+
+int calculateDuration(int startTime, int endTime) {
+  // Handling cases where the shift crosses over to the next day
+  if (endTime < startTime) {
+    // Calculate duration for the remaining hours of the first day
+    int remainingHoursOfDay1 = 24 - startTime;
+
+    // Add the start time of the next day to calculate the total duration
+    return remainingHoursOfDay1 + endTime;
+  } else {
+    // Calculate duration for a shift within the same day
+    return endTime - startTime;
+  }
 }
